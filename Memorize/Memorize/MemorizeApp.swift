25b64bd5d29d79c716dcf0d628d0006e107c80cb
Atanas Kolev <atanaskolev@Atanass-MacBook-Air.local>
2021-09-10 10:21:05 +0300

    initial commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Atanas Kolev on 10.09.21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
