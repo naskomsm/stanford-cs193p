596dda6b8b6e911dde8bddfa647a2488b74015b1
naskomsm <naskokolev00@gmail.com>
2021-10-01 13:31:58 +0300

    added remove/add cards to the game

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,18 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚗", "🚕", "🚙", "🚌",
+                  "🚎", "🏎", "🚓", "🚑",
+                  "🚒", "🚐", "🛻", "🚚",
+                  "🚛", "🚜", "🛴", "🚲",
+                  "🛵", "🏍", "🛺" , "🚔",
+                  "🚍", "🚘", "🚖", "✈️"]
+    
+    @State var emojiCount = 4
+    
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 30)
-                .stroke(lineWidth: 3)
+        VStack {
+            HStack {
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                }
+            }
             
-            Text("Hello World!")
-        }.padding(.horizontal).foregroundColor(.red)
+            HStack {
+                Button(action: {
+                    emojiCount -= 1
+                }, label: {
+                    VStack {
+                        Text("Remove")
+                        Text("Card")
+                    }
+                })
+                
+                Button(action: {
+                    emojiCount += 1
+                }, label: {
+                    VStack {
+                        Text("Add")
+                        Text("Card")
+                    }
+                })
+            }
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack{
+            let shape = RoundedRectangle(cornerRadius: 30)
+            
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+
+            } else {
+                shape.fill()
+            }
+        }.onTapGesture{
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
